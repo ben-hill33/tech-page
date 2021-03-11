dc163ce4ad13dbfccc7fda74d91d3389612c1080
BEN <benjamin.hill28@gmail.com>
2021-03-10 19:19:33 -0800

    added post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now)
+    pub_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        """ Publish date can be empty and doesnt have to have any characters """
+        self.pub_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.approve_comments.filter(approved_comments=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("post_detail", args=[str(self.id)])
